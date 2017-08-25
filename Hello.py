80b185e882ec5502ea691494138acc5f7e5659fe
Ewerton Lima <erlima@erlima.local>
2017-08-25 13:52:50 -0300

    teste

--- Hello.py
+++ Hello.py
@@ -1,24 +1,25 @@
 import random
+
 def main ():
-	
+
 	print "Este jogo ajuda voce a adivinhar um numero"
 	print "Digite um numero e depois Enter"
 	randonnumber = random.randint(1,100)
 	found = False # flag variable
 
 	while not found:
-		
+
 		userguess = input("Digite o numero:  ")
 		if userguess == randonnumber:
 			print "Parabens esquilinho o numero era" randonnumber
 			found = True
 			print "Obrigado por jogar conosco"
-			
+
 		elif	userguess > randonnumber:
 			print "Tente um numero MENOR"
 		else: print "Tente um numero MAIOR"
-	
-	
-	
+
+
+
 if __name__ == "__main__":
 	main()
